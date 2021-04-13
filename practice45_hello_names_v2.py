325a9842536be3b291f2214ee20a9d72383abada
Manu Siddharth Basotia <msbasotia@gmail.com>
2021-04-13 07:37:06 +0530

    lists2

--- practice45_hello_names_v2.py
+++ practice45_hello_names_v2.py
@@ -5,16 +5,14 @@ def wish(x):
     y = 0
     while y < 4:
         print(f"{wishes[y]} {names[x]}.")
-        y = y+1 # even if I comment this line, code still runs. Why?
+        y = y+1
 
 
 def main():
     x = 0
     while x < 4:
         wish(x)
-        x = x+1 # even if I comment this line, code still runs. Why?
+        x = x+1
 
 main()        
 
-
-# when running a while within while, the "+1" conditions are required but. Why?
